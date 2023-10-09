6fee76be0cbc088469a289afe2554842958fef2f
salma <85360667+salma-mohamed24@users.noreply.github.com>
2023-10-09 17:33:48 +0300

    Add files via upload

---
+++ Models/main.py
@@ -0,0 +1,20 @@
+from Violation import violation
+import Services
+
+# Rest of your code ...
+
+
+if __name__ == '__main__':
+    Services.app.run(debug=True)
+    #retrieve_attributes()
+    #platenumber='98765432890643'
+    #getVideoData()
+    
+    #array_of_arrays=[["no damage", "brown", "car", "دع١٢٦٨", 47], ["no damage", "white", "car", "دل٤٦٢٥", 19], ["no damage", "grey", "car", "رن٦٢١٣", 12], ["no damage", "white", "car", "طي٤٥٣١", 9], ["smash", "white", "car", "اوج٧٨٢٣", 8]]
+    # violation_instance = violation()  # Create an instance of the Violation class
+    # violation_instance.giveTrafficViolation(array_of_arrays)  # Call the method on the instance
+    #violation.giveTrafficViolation(array_of_arrays)
+
+    #getGovernmentData()
+    #getLastVideo()
+   # getGovernmentData("98765432890643")# your code goes here
